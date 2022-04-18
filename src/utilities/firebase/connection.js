617375c9b28d64e1a69a51c317a24fa17c7499f4
LinkFalcon0921 <elioerickramos@gmail.com>
2022-04-18 12:15:04 -0400

    Better implementations

--- src/utilities/firebase/connection.js
+++ src/utilities/firebase/connection.js
@@ -7,13 +7,22 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 console.log(process.env.REACT_APP_FIREBASE_API_KEY);
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyCGS46yIj5t11ZB1Q4QypLcygMUPigdwv4',
+  authDomain: 'react-app-js-d04b2.firebaseapp.com',
+  projectId: 'react-app-js-d04b2',
+  storageBucket: 'react-app-js-d04b2.appspot.com',
+  messagingSenderId: '1085514883737',
+  appId: '1:1085514883737:web:3dc944a6ed150272e04e08',
 };
 
 // Initialize Firebase
